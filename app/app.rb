e89fb8a2660b788a48dbe100010c13d8089d57a8
Kirsten Jones <kirstenjones@yahoo.co.uk>
2015-06-29 18:03:32 +0100

    fix 200 500 error

--- app/app.rb
+++ app/app.rb
@@ -2,4 +2,12 @@ require 'sinatra/base'
 
 class BManager < Sinatra::Base
 
+  set :views, proc { File.join(root, '..', 'view') }
+
+  get '/links' do
+    @links = Link.all
+    erb :'links/index'
+  end
+
+  run! if app_file == $0
 end
